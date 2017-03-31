616f7f2e47d80e4b30c3f7730345053b9578be97
Keita Kasai <k013c1345@it-neec.jp>
2017-03-31 21:50:40 +0900

    Repositoryクラスのジェネリクスのミスの修正

--- src/main/java/com/example/project/domain/InquiryRepository.java
+++ src/main/java/com/example/project/domain/InquiryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InquiryRepository extends JpaRepository<InquiryEntity, Integer> {
+public interface InquiryRepository extends JpaRepository<InquiryEntity, Long> {
 }
